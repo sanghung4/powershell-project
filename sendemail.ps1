9945fa27ad3b4da49c4a538843f1c971188a0355
Hung Sang <hungsang@ppi-cg3yemd6r.attlocal.net>
2022-03-02 15:50:06 -0600

    added sendemail api

---
+++ sendemail.ps1
@@ -0,0 +1,91 @@
+Function SendGridMailWithAttachment {
+    param (
+        [cmdletbinding()]
+        [parameter()]
+        [string]$ToAddress,
+        [parameter()]
+        [string]$FromAddress,
+        [parameter()]
+        [string]$Subject,
+        [parameter()]
+        [string]$Body,
+        [parameter()]
+        [string]$APIKey,
+        [parameter()]
+        [string]$FileName,
+        [parameter()]
+        [string]$FileNameWithFilePath,
+		[parameter()]
+        [string]$AttachementType
+    )
+
+    #Convert File to Base64
+    $FileContent = get-content $FileNameWithFilePath
+    $ConvertToBytes = [System.Text.Encoding]::UTF8.GetBytes($FileContent)
+    $EncodedFile = [System.Convert]::ToBase64String($ConvertToBytes)
+
+    # Body with attachement for SendGrid
+    $SendGridBody = @{
+        "personalizations" = @(
+            @{
+                "to"= @(
+                              @{
+                                   "email" = $ToAddress
+                               }
+                 )
+
+                "subject" = $Subject
+            }
+        )
+
+                "content"= @(
+                              @{
+                                    "type" = "text/html"
+                                    "value" = $Body
+                               }
+                 )
+
+                "from"  = @{
+                            "email" = $FromAddress
+                           }
+
+                "attachments" = @(
+                                    @{
+                                        "content"=$EncodedFile
+                                        "filename"=$FileName
+                                        "type"= $AttachementType
+                                        "disposition"="attachment"
+                                     }
+               )
+}
+
+    $BodyJson = $SendGridBody | ConvertTo-Json -Depth 4
+
+
+    #Header for SendGrid API
+    $Header = @{
+        "authorization" = "Bearer $APIKey"
+    }
+
+    #Send the email through SendGrid API
+    $Parameters = @{
+        Method      = "POST"
+        Uri         = "https://api.sendgrid.com/v3/mail/send"
+        Headers     = $Header
+        ContentType = "application/json"
+        Body        = $BodyJson
+    }
+    Invoke-RestMethod @Parameters
+}
+
+$Parameters = @{
+    ToAddress   = "sangvhung@gmail.com"
+    FromAddress = "exthung.sang@bcg.com"
+    Subject     = "Powershell script test result report"
+    Body        = "Build Completed"
+    APIKey       = "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"
+    FileName ="emailable-report.html"
+    FileNameWithFilePath = "/Users/hungsang/Documents/JavaProjectDemo/target/surefire-reports/emailable-report.html"
+    AttachementType ="text/html"
+}
+SendGridMailWithAttachment @Parameters
