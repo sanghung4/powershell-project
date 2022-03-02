d9aef81ba1583f2817436807b87b015cafac056d
MattMcHattonBCG <extMcHatton.Matt@bcg.com>
2022-03-02 13:12:58 -0600

    adding menu structure

--- testing.ps1
+++ testing.ps1
@@ -1,8 +1,100 @@
-$tests = Read-Host "Which tests would you like to run"
-$svc = Read-Host "Which services would you like to test"
-$env = Read-Host "Which environment would you like to test in"
+
+
+# create if for loop to go to the next prompt
+$continue = $false
+While (!$continue) {
+    $tests = Read-Host "Which tests would you like to run?"
+    if ($tests -eq "1") {
+        Write-Host "Script:" $PSCommandPath
+        Write-Host "Path:" $PSScriptRoot 
+        $continue = $true
+        }
+    #ElseIf ($tests -eq "2") {
+        #Write-Host "Script:" $PSCommandPath
+        #Write-Host "Path:" $PSScriptRoot }
+    elseif ($tests -eq "Q") {
+        Write-Host "Exiting"
+        Exit
+    }
+    else {
+        Write-Host "Please select a valid value"
+    }
+}
+
+$continue = $false
+While (!$continue) {
+    $svc = Read-Host "Which services would you like to test?"
+    if ($svc -eq "1") {
+        Write-Host "Script:" $PSCommandPath
+        Write-Host "Path:" $PSScriptRoot 
+        $continue = $true
+        }
+    #ElseIf ($tests -eq "2") {
+        #Write-Host "Script:" $PSCommandPath
+        #Write-Host "Path:" $PSScriptRoot }
+    elseif ($svc -eq "Q") {
+        Write-Host "Exiting"
+        Exit
+    }
+    else {
+        Write-Host "Please select a valid value"
+    }
+}
+
+$continue = $false
+While (!$continue) {
+    $env = Read-Host "Which environment would you like to test in"
+    if ($env -eq "1") {
+        Write-Host "Script:" $PSCommandPath
+        Write-Host "Path:" $PSScriptRoot 
+        $env = "Development"
+        $continue = $true
+        }
+    elseif ($env -eq "2") {
+        Write-Host "Script:" $PSCommandPath
+        Write-Host "Path:" $PSScriptRoot 
+        $env = "QA"
+        $continue = $true
+        }
+    elseif ($env -eq "3") {
+        Write-Host "Script:" $PSCommandPath
+        Write-Host "Path:" $PSScriptRoot 
+        $env = "azure-pipeline"
+        $continue = $true
+        }
+    elseif ($env -eq "Q") {
+        Write-Host "Exiting"
+        Exit
+    }
+    else {
+        Write-Host "Please select a valid value"
+    }
+}
 
 Write-Output "Test selected: $tests, service selected $svc, environment selected $env"
+
+# do {
+#     Write-Host "`n============= Pick the Environment=============="
+#     Write-Host "`ta. '1' for the Prod Environment"
+#     Write-Host "`tb. '2' for the QA Environment"
+#     Write-Host "`td. 'Q' to Quit'"
+#     Write-Host "========================================================"
+#     $choice = Read-Host "`nEnter Choice"
+#     } until (($choice -eq '1') -or ($choice -eq '2') -or ($choice -eq 'Q') )
+#     switch ($choice) {
+#        '1'{
+#            Write-Host "`nYou have selected a Prod Environment"
+#        }
+#        '2'{
+#           Write-Host "`nYou have selected a Test Environment"
+#        }
+#        'Q'{
+#            Write-Host "`nYou have selected to Quit",
+#           Return
+#        }
+    #}
+
+
 
 Write-Output "Beginning Tests..."
 
@@ -14,9 +106,10 @@ $headers = @{
     "Authorization"="Basic $($authorizationInfo)"
     "Content-Type"="application/json"
     }
-$body = “{`n    `“resources`“: {`n        `“repositories`“: {`n            `“self`“: {`n                `“refName`“: `“refs/heads/azure-pipeline`“`n            }`n        }`n    },    `n    `“templateParameters`“: {`n        `“pomFile`“: `“pom.xml`“`n    }`n}”
+$body = “{`n    `“resources`“: {`n        `“repositories`“: {`n            `“self`“: {`n                `“refName`“: `“refs/heads/$env`“`n            }`n        }`n    },    `n    `“templateParameters`“: {`n        `“pomFile`“: `“pom.xml`“`n    }`n}”
 
-$response = Invoke-RestMethod ‘https://dev.azure.com/extHungSang/SonarCubeExample/_apis/pipelines/3/runs?api-version=6.0-preview.1’ -Method ‘POST’ -Headers $headers -Body $body
-$response | ConvertTo-Json
+#$response = Invoke-RestMethod ‘https://dev.azure.com/extHungSang/SonarCubeExample/_apis/pipelines/3/runs?api-version=6.0-preview.1’ -Method ‘POST’ -Headers $headers -Body $body
+#$response | ConvertTo-Json
 
-Write-Output $response
+Write-Output $body
+Write-Output "Done"
